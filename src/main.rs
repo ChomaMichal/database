4a1bddb544c7540dd3007613968f87b87c31e2b9
Michal Choma <michalkk04@gmail.com>
2026-04-22 13:06:29 +0200

    insertion is working

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
-use std::env;
-use std::fs::File;
-use std::fs::OpenOptions;
-use std::os::unix::fs::FileExt;
+//use rand::prelude::*;
+//use std::env;
+//use std::fs::File;
+//use std::fs::OpenOptions;
+//use std::os::unix::fs::FileExt;
 pub mod btree;
-pub mod table;
-use table::PAGE;
-fn main() {}
+use btree::BTree;
+
+fn main() {
+    //    let mut rng = rand::rng();
+    let mut btree = BTree::<u32>::new();
+    let mut arr: Vec<u32> = Vec::<u32>::new();
+    for i in 0..11 {
+        let tmp: u32 = i; // rng.random();
+
+        arr.push(tmp);
+        btree.insert(tmp);
+        println!("{:?}", btree);
+        println!("===============");
+    }
+    // println!("{:?}", btree.elements);
+    // println!("{:?}", btree.pointers);
+    for item in arr.iter_mut() {
+        let res = btree.find(*item);
+        match res {
+            Some(el) => {
+                if el != item {
+                    eprintln!("Found incorrect number {}  != {}", item, el);
+                }
+            }
+            None => eprintln!("Number {} not found", item),
+        }
+    }
+}
