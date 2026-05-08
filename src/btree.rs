8ab9a33fb7cf40e7a5a2eba1015aed5a95177f0b
Michal Choma <michalkk04@gmail.com>
2026-05-08 20:23:38 +0200

    insertion and finding working

--- src/btree.rs
+++ src/btree.rs
@@ -1,3 +1,4 @@
+use std::fmt::{self, Display, Formatter};
 use std::mem::swap;
 
 const ORDER: usize = 5;
@@ -32,7 +33,7 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> OverFlow<T
     }
 }
 
-//takes reffferences to option box arrays and  swaps first len element
+//takes reffreferences to option box arrays and swaps first len element
 fn arr_swap<T>(src: &mut [T], dest: &mut [T], len: usize) {
     for i in 0..len {
         swap(&mut src[i], &mut dest[i]);
@@ -88,7 +89,10 @@ pub fn test_arr_swap() {
     assert_eq!(right, exp_right);
 }
 
-impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
+impl<T> BTree<T>
+where
+    T: PartialOrd + PartialEq + Display + std::fmt::Debug,
+{
     pub fn new() -> Self {
         Self {
             elements: [const { None }; ORDER],
@@ -105,44 +109,60 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
                 btree.pointers[0] = tmp.left;
                 btree.pointers[1] = tmp.right;
                 swap(self, &mut btree);
-                // println!("{:?} == top", self);
-                // println!("=====================================");
                 return;
             }
             None => return,
         }
     }
-
-    pub fn find(&self, element: T) -> Option<&T> {
+    fn _len(&self) -> usize {
         let len = self.elements.iter().position(|e| e == &None);
-        let len = match len {
+        match len {
             Some(l) => l,
             None => ELEMENTS_LEN,
-        };
-        let mut top_pos = len;
-        let mut bot_pos = 0;
+        }
+    }
 
+    pub fn find(&self, element: T) -> Option<&T> {
+        let len = self._len();
+        if len == 0 {
+            return None;
+        }
+        let mut top_pos = len - 1;
+        let mut bot_pos = 0;
+        let mut prev = ORDER + 1;
         loop {
             let pos = bot_pos + (top_pos - bot_pos) / 2;
-            if pos == bot_pos && pos == top_pos {
-                match self.pointers[pos].as_ref() {
-                    Some(child) => {
-                        return child.find(element);
-                    }
-                    None => {
-                        if **self.elements[pos].as_ref().unwrap() == element {
-                            return Some(self.elements[pos].as_ref().unwrap());
-                        }
-                        return None;
-                    }
-                }
-            } else if **self.elements[pos].as_ref().unwrap() < element {
-                top_pos = top_pos - (top_pos - bot_pos) / 2;
-            } else if **self.elements[pos].as_ref().unwrap() > element {
-                bot_pos = bot_pos + (top_pos - bot_pos) / 2;
-            } else if **self.elements[pos].as_ref().unwrap() == element {
+            if **self.elements[pos].as_ref().unwrap() == element {
                 return Some(self.elements[pos].as_ref().unwrap());
             }
+            if bot_pos == top_pos {
+                if **self.elements[len - 1].as_ref().unwrap() < element {
+                    top_pos += 1;
+                }
+                match self.pointers[top_pos].as_ref() {
+                    Some(pointer) => return pointer.find(element),
+                    None => return None,
+                }
+            }
+
+            if **self.elements[pos].as_ref().unwrap() < element {
+                if bot_pos == 0 && top_pos == 1 {
+                    bot_pos = 1;
+                } else {
+                    bot_pos = pos;
+                }
+            } else if **self.elements[pos].as_ref().unwrap() > element {
+                top_pos = pos;
+            }
+            if pos == prev {
+                if pos == bot_pos {
+                    bot_pos += 1;
+                }
+                if pos == top_pos {
+                    top_pos -= 1;
+                }
+            }
+            prev = pos;
         }
     }
 
@@ -160,7 +180,7 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
             1 => {
                 arr_swap(&mut self.elements, &mut left.elements, MIDLE);
                 arr_swap(&mut self.elements[MIDLE + 1..], &mut right.elements, MIDLE);
-                arr_swap(&mut self.pointers, &mut left.pointers, MIDLE);
+                arr_swap(&mut self.pointers, &mut left.pointers, MIDLE + 1);
                 arr_swap(&mut self.pointers[MIDLE + 1..], &mut right.pointers, MIDLE);
             }
             0 => {
@@ -181,7 +201,7 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
         }
         let mut tmp = None;
         swap(&mut self.elements[MIDLE], &mut tmp);
-        return OverFlow::new(tmp, Some(left), Some(right));
+        OverFlow::new(tmp, Some(left), Some(right))
     }
 
     fn _insert(&mut self, element: T) -> Option<OverFlow<T>> {
@@ -216,17 +236,13 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
                     if **el == element {
                         println!("Duplicates");
                     } else if element < **el {
-                        println!("enters 186");
                         match self.pointers[i].as_mut() {
-                            //if it has no children
                             None => {
                                 let tmp = self._is_leaf_full();
                                 match tmp {
-                                    //do the splitting here
                                     true => {
                                         return Some(self._split_node());
                                     }
-
                                     false => {
                                         self.elements[i..].rotate_right(1);
                                         self.elements[i] = Some(Box::new(element));
@@ -235,9 +251,7 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
                                     }
                                 }
                             }
-                            //if it has children
                             Some(ptr) => {
-                                print!("Inserting to children");
                                 match ptr._insert(element) {
                                     Some(extra) => match self._is_leaf_full() {
                                         false => {
@@ -255,9 +269,34 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
                 }
             };
         }
-        let mut tmp = self._split_node();
-        tmp.right.as_mut().unwrap()._insert(element);
-        return Some(tmp);
+        match self.pointers[POINTERS_LEN - 1].as_mut() {
+            Some(el) => match el._insert(element) {
+                Some(extra) => match self._is_leaf_full() {
+                    true => {
+                        let mut n_extra = self._split_node();
+                        let index = extra.right.as_ref().unwrap()._len() - 1;
+                        n_extra
+                            .right
+                            .as_mut()
+                            .unwrap()
+                            .as_mut()
+                            ._insert_overflow_at_index(index, extra);
+                        return Some(n_extra);
+                    }
+                    false => {
+                        let index = extra.right.as_ref().unwrap()._len() - 1;
+                        self._insert_overflow_at_index(index, extra);
+                        return None;
+                    }
+                },
+                None => return None,
+            },
+            None => {
+                let mut tmp = self._split_node();
+                tmp.right.as_mut().unwrap()._insert(element);
+                return Some(tmp);
+            }
+        }
     }
 
     //check if the node is full before calling
@@ -268,4 +307,45 @@ impl<T: PartialOrd + PartialEq + std::fmt::Display + std::fmt::Debug> BTree<T> {
         self.pointers[i..].rotate_right(1);
         swap(&mut self.pointers[i], &mut extra.left);
     }
+
+    fn fmt_pretty_with_indent(&self, depth: usize, out: &mut String) {
+        let indent = "  ".repeat(depth);
+
+        out.push_str(&format!("{indent}BTreeNode {{\n"));
+
+        out.push_str(&format!("{indent}  elements: ["));
+        let mut first = true;
+        for e in &self.elements {
+            if !first {
+                out.push_str(", ");
+            }
+            first = false;
+            match e {
+                Some(v) => out.push_str(&format!("{v}")),
+                None => out.push_str("_"),
+            }
+        }
+        out.push_str("]\n");
+
+        for (i, ptr) in self.pointers.iter().enumerate() {
+            if let Some(child) = ptr.as_ref() {
+                out.push_str(&format!("{indent}  child[{i}]:\n"));
+                child.fmt_pretty_with_indent(depth + 1, out);
+            }
+        }
+
+        out.push_str(&format!("{indent}}}\n"));
+    }
 }
+
+impl<T> Display for BTree<T>
+where
+    T: PartialOrd + PartialEq + Display + std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut out = String::new();
+        self.fmt_pretty_with_indent(0, &mut out);
+        write!(f, "{out}")
+    }
+}
+
