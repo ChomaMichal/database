204a14f3c6242cf44fd1ae260f7cea6a001025e4
Michal Choma <michalkk04@gmail.com>
2026-05-19 11:13:37 +0200

    feature: added serialization, disc page operations and metadata creation

--- src/main.rs
+++ src/main.rs
@@ -14,44 +14,4 @@ mod btree_tests;
 use btree::BTree;
 pub mod table;
 
-fn main() {
-    let mut rand = StdRng::seed_from_u64(1);
-    // let mut rng = rand::rng();
-    let mut btree = BTree::<u32>::new();
-    let mut arr: Vec<u32> = Vec::<u32>::new();
-    for i in (0..71) {
-        let tmp: u32 = rand.random();
-        let tmp = tmp % 1000;
-
-        // let tmp = i;
-
-        arr.push(tmp);
-        if tmp == 656 {
-            println!("stop here");
-        }
-        btree.insert(tmp);
-        println!("==========element{tmp}==============");
-        println!("{}", btree);
-    }
-    // println!("{}", btree);
-    // let tmp = btree.find(6);
-    // println!("return {:?}", tmp);
-    // println!("{:?}", btree.elements);
-    // println!("{:?}", btree.pointers);
-    for item in arr.iter_mut() {
-        let res = btree.find(*item);
-        match res {
-            Some(el) => {
-                if el != item {
-                    println!("Found incorrect number {}  != {} !!", item, el);
-                    panic!();
-                }
-                println!("Found the numbner {el}");
-            }
-            None => {
-                println!("Number {} not found !!", item);
-                panic!()
-            }
-        }
-    }
-}
+fn main() {}
--- src/table.rs
+++ src/table.rs
@@ -1,7 +1,16 @@
+use bincode;
+use bincode::de;
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+#[path = "disc/disc.rs"]
+pub mod disc;
+use disc::Disc;
+use std::f32::consts::TAU;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io;
 use std::io::prelude::*;
+use std::ops::Index;
 use std::os::unix::fs::FileExt;
 use std::path::Path;
 
@@ -28,29 +37,296 @@ pub const PAGE: usize = 8096;
 * figure out how to handle collum insertions without copying the whole table or how it might be OK
 */
 
-//might change later
+#[derive(Serialize, Deserialize)]
+struct DiscString {
+    next: Option<u64>, //where the string might continue
+    string: String,    // the string
+}
 
-#[derive(Debug, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
+#[repr(u8)]
+pub enum ColTypes {
+    Empty = 0,
+    I32 = 1,
+    U32 = 2,
+    I64 = 3,
+    U64 = 4,
+    String = 5,
+}
+
+#[derive(Serialize_repr, Deserialize_repr, Copy, Clone, Debug)]
+#[repr(u8)]
+pub enum PageType {
+    Empty = 0,    //page is currently empty
+    Btree = 1,    //pagee containts btree node
+    Data = 2,     // page contains data
+    MetaData = 3, // page contains data about the table
+}
+
+//first page always contains metadata ther will be pointerst to all columns and indexes
+#[derive(Serialize, Deserialize)]
+#[repr(u8)]
+enum MetaDataTypes {
+    End = 0,    //metadata ended
+    Column = 1, //len of the next column
+    Index = 2,  //len of next index (index is for example the btree and key based on which it is
+    //created)
+    NextPage = 3, //index of next page with metadata
+}
+
+#[derive(Serialize, Deserialize)]
+struct Indexing {
+    name: String,    //name by which is the index created
+    page_index: u64, // which page contains the head of index
+}
+
+#[derive(Serialize, Deserialize)]
+struct Column {
+    col_type: ColTypes,
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DataHeader {
+    offset: u64, //how far from index does it begin
+    len: u64,    //how long is it
+}
+
+#[derive(Serialize, Deserialize)]
+struct PageHeader {
+    page_type: PageType,
+}
+
 struct Table {
-    name: String,      //to find the file in question
-    rows: Vec<String>, // names of the columns index is important because cells use this indexing
-    //this will contain the b tree for searching elements
-    free_page: u64,
+    disc: Disc,
+    indexes: Vec<Indexing>,
+    columns: Vec<Column>,
 }
 
-#[derive(Debug, Clone)]
-struct Data {
-    pointer: u64, //contains the pointer to the data in context of this page
-    len: u64,     //length of data might delete later
+impl Table {
+    fn new(disc: Disc) -> Self {
+        Self {
+            disc: disc,
+            indexes: vec![],
+            columns: vec![],
+        }
+    }
+
+    //call before returning new table
+    fn init_metadata(&mut self) -> Option<()> {
+        let mut buf = [0u8; PAGE];
+        let mut index = 0;
+        let mut current_page = 0;
+        self.disc.new_page();
+        buf[0] = PageType::MetaData as u8;
+        index += 1;
+        for it in self.columns.iter() {
+            let len = it.name.as_bytes().len() + 13; // 1 because of the type 4 becasue of the
+            // len fo the index 4 because of the len fo the string 4 because of the head of indexand len of the string
+            if index + len + 9 > PAGE {
+                // i think it should be + 5 becasue one of type4 of the
+                // index
+                let new_page = self.disc.new_page().unwrap();
+                serialize_metadata_next_page(&mut buf, &mut index, new_page);
+                self.disc.write_to_page(current_page, &mut buf).unwrap();
+                index = 0;
+                current_page = new_page;
+            }
+            serialize_metadata_column(&mut buf, &mut index, it);
+        }
+
+        for it in self.indexes.iter() {
+            let len = it.name.as_bytes().len() + 13; // 1 because of the type 4 becasue of the
+            // len fo the index 4 because of the len fo the string 4 because of the head of indexand len of the string
+            if index + len + 9 > PAGE {
+                // i think it should be + 5 becasue one of type4 of the
+                // index
+                let new_page = self.disc.new_page().unwrap();
+                serialize_metadata_next_page(&mut buf, &mut index, new_page);
+                self.disc.write_to_page(current_page, &mut buf).unwrap();
+                index = 0;
+                current_page = new_page;
+            }
+            serialize_metadata_index(&mut buf, &mut index, it);
+        }
+        serialize_metadata_end(&mut buf, &mut index);
+        self.disc.write_to_page(current_page, &mut buf).unwrap();
+        return Some(());
+    }
+
+    fn deserialize_metadata(mut self) -> Self {
+        let mut buf = [0u8; PAGE];
+        let mut index = 0;
+        let mut current_page = 0;
+        self.disc.get_page(0, &mut buf);
+        let tmp = deserialize_u8(&mut buf, &mut index);
+        assert!(tmp == PageType::MetaData as u8);
+        loop {
+            let htype = u8to_metadatatype(deserialize_u8(&mut buf, &mut index));
+            match htype {
+                MetaDataTypes::End => break,
+                MetaDataTypes::Column => {
+                    self.columns
+                        .push(deserialize_metadata_column(&mut buf, &mut index));
+                }
+                MetaDataTypes::Index => {
+                    self.indexes
+                        .push(deserialize_metadata_index(&mut buf, &mut index));
+                }
+                MetaDataTypes::NextPage => {
+                    self.disc
+                        .get_page(
+                            deserialize_metadata_next_page(&mut buf, &mut index) as usize,
+                            &mut buf,
+                        )
+                        .unwrap();
+                }
+            }
+        }
+        return self;
+    }
+
+    pub fn open_table(name: String) -> Option<Self> {
+        let dsc = match Disc::open_disc(&name) {
+            Ok(tmp) => tmp,
+            Err(_) => return None,
+        };
+        let table = Table::new(dsc);
+        Some(table.deserialize_metadata())
+    }
+
+    pub fn create_table(name: String, columns: Vec<Column>) -> Option<Self> {
+        let dsc = match Disc::create_disc(&name) {
+            Some(tmp) => tmp,
+            None => return None,
+        };
+        let mut table = Table {
+            disc: dsc,
+            columns: columns,
+            indexes: vec![],
+        };
+        if table.init_metadata().is_none() {
+            Disc::delete_discs(&name);
+            return None;
+        }
+        return Some(table);
+    }
 }
 
-#[derive(Debug, Clone)]
-struct Cell {
-    index: u64,      //contains the index of the row being quueried
-    rows: Vec<Data>, //contains pinter to begining of the data in the same indexing as in the vec
-    //in table
-    len: u64,
-    //cont:  OverflowCell, //figure out how tf to handle overflow cell
+fn serialize_string(buf: &mut [u8], index: &mut usize, string: &String) {
+    let len = string.as_bytes().len() as u32;
+    buf[*index..*index + 4].copy_from_slice(&len.to_le_bytes()); //len of the string (u32)
+    *index += 4;
+    buf[*index..*index + len as usize].copy_from_slice(string.as_bytes()); //string
+    *index += len as usize;
+}
+
+fn serialize_u8(buf: &mut [u8], index: &mut usize, el: u8) {
+    buf[*index] = el;
+    *index += 1;
+}
+
+fn serialize_u64(buf: &mut [u8], index: &mut usize, el: u64) {
+    buf[*index..*index + 8].copy_from_slice(&el.to_le_bytes());
+    *index += 8;
+}
+
+fn deserialize_string(buf: &mut [u8], index: &mut usize) -> String {
+    let len_bytes: [u8; 4] = buf[*index..*index + 4].try_into().unwrap();
+    let len = u32::from_le_bytes(len_bytes) as usize;
+    *index += 4;
+    let s = std::str::from_utf8(&buf[*index..*index + len])
+        .expect("invalid utf-8 in deserialize_string")
+        .to_string();
+    *index += len;
+    s
+}
+
+fn deserialize_u8(buf: &mut [u8], index: &mut usize) -> u8 {
+    let ret = buf[*index];
+    *index += 1;
+    ret
+}
+
+fn deserialize_u64(buf: &mut [u8], index: &mut usize) -> u64 {
+    let ret_bytes: [u8; 4] = buf[*index..*index + 4].try_into().unwrap();
+    let ret = u32::from_le_bytes(ret_bytes) as usize;
+    *index += 1;
+    ret as u64
+}
+
+fn u8to_metadatatype(i: u8) -> MetaDataTypes {
+    match i {
+        0 => MetaDataTypes::End,
+        1 => MetaDataTypes::Column,
+        2 => MetaDataTypes::Index,
+        3 => MetaDataTypes::NextPage,
+        _ => panic!("fucked up metadatatype"),
+    }
+}
+
+fn u8to_coltype(i: u8) -> ColTypes {
+    match i {
+        0 => ColTypes::Empty,
+        1 => ColTypes::I32,
+        2 => ColTypes::U32,
+        3 => ColTypes::I64,
+        4 => ColTypes::U64,
+        5 => ColTypes::String,
+        _ => panic!("fucked up col_type"),
+    }
+}
+
+fn serialize_metadata_end(buf: &mut [u8], index: &mut usize) {
+    serialize_u8(buf, index, MetaDataTypes::End as u8);
+}
+
+fn serialize_metadata_next_page(buf: &mut [u8], index: &mut usize, new_page: usize) {
+    serialize_u8(buf, index, MetaDataTypes::NextPage as u8);
+    serialize_u64(buf, index, new_page as u64);
+}
+
+fn deserialize_metadata_next_page(buf: &mut [u8], index: &mut usize) -> u64 {
+    deserialize_u64(buf, index)
+}
+
+fn serialize_metadata_index(buf: &mut [u8], index: &mut usize, i: &Indexing) {
+    let len = i.name.as_bytes().len() + 13;
+    serialize_u8(buf, index, MetaDataTypes::Index as u8); //type of metadata
+    serialize_u64(buf, index, len as u64); //len of index metadata
+    serialize_u64(buf, index, i.name.as_bytes().len() as u64); //len of
+    //string
+    serialize_string(buf, index, &i.name); // string
+    serialize_u64(buf, index, i.page_index as u64); //head of index
+}
+
+fn deserialize_metadata_index(buf: &mut [u8], index: &mut usize) -> Indexing {
+    let _ = deserialize_u64(buf, index); // deserializing len of index in metadata
+    let string = deserialize_string(buf, index);
+    let page_index = deserialize_u64(buf, index);
+    Indexing {
+        name: string,
+        page_index: page_index,
+    }
+}
+fn serialize_metadata_column(buf: &mut [u8], index: &mut usize, column: &Column) {
+    let len = column.name.as_bytes().len() + 13;
+    serialize_u8(buf, index, MetaDataTypes::Column as u8); //type of metadata
+    serialize_u64(buf, index, len as u64); //len of index metadata
+    serialize_u64(buf, index, column.name.as_bytes().len() as u64); //len of
+    //string
+    serialize_string(buf, index, &column.name); // string
+    serialize_u8(buf, index, column.col_type.clone() as u8); //head of index
+}
+
+fn deserialize_metadata_column(buf: &mut [u8], index: &mut usize) -> Column {
+    let _ = deserialize_u64(buf, index); // deserializing len of index in metadata
+    let string = deserialize_string(buf, index);
+    let col_type = deserialize_u8(buf, index);
+    Column {
+        name: string,
+        col_type: u8to_coltype(col_type),
+    }
 }
 
 /*
@@ -59,58 +335,3 @@ struct OverflowCell {
     cell_index: u64,
 }
 */
-
-pub enum Types {
-    Empty,
-    I32(i32),
-    U32(u32),
-    I64(i64),
-    U64(u64),
-    String(String),
-}
-pub type Row = Vec<Types>;
-
-fn get_table(name: &String) -> Result<File, std::io::Error> {
-    OpenOptions::new().write(true).read(true).open(name)
-}
-
-pub fn add_column(name: &String, data: Row) -> Result<(), ()> {
-    let mut file = match get_table(name) {
-        Ok(val) => val,
-        Err(e) => return Err(()),
-    };
-    return Err(());
-}
-
-pub fn create_table(name: &String, columns: &Vec<String>) -> Result<(), String> {
-    if Path::new(name).exists() == true {
-        return Err("Table already exists".to_string());
-    }
-    let mut file = match OpenOptions::new().write(true).create_new(true).open(name) {
-        Ok(val) => val,
-        Err(err) => {
-            return Err(err.to_string());
-        }
-    };
-
-    let mut buffer: [u8; PAGE] = [0; PAGE];
-
-    let mut offset = 0usize;
-    for s in columns {
-        let b = s.as_bytes();
-        buffer[offset..offset + b.len()].copy_from_slice(b);
-        offset += b.len();
-        buffer[offset] = b'\n';
-        offset += 1;
-    }
-
-    match file.write_all(&buffer) {
-        Ok(()) => {
-            println!("Table created succsessfuly");
-        }
-        Err(val) => {
-            println!("Failed to write to a file: {}", val);
-        }
-    }
-    return Ok(());
-}
